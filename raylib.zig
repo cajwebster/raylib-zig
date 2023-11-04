5bcc1aa164c133dcb85b861c2b624c5a85a075da
Christopher Webster <cajwebster03@gmail.com>
2023-11-04 10:26:55 -0300

    window flags

--- raylib.zig
+++ raylib.zig
@@ -319,6 +319,36 @@ pub const RAYWHITE = Color{ .r = 245, .g = 245, .b = 245, .a = 255 }; // My own 
 
 // Constants
 
+/// Set to try enabling V-Sync on GPU
+pub const FLAG_VSYNC_HINT: c_int = 0x00000040;
+/// Set to run program in fullscreen
+pub const FLAG_FULLSCREEN_MODE: c_int = 0x00000002;
+/// Set to allow resizable window
+pub const FLAG_WINDOW_RESIZABLE: c_int = 0x00000004;
+/// Set to disable window decoration (frame and buttons)
+pub const FLAG_WINDOW_UNDECORATED: c_int = 0x00000008;
+/// Set to hide window
+pub const FLAG_WINDOW_HIDDEN: c_int = 0x00000080;
+/// Set to minimize window (iconify)
+pub const FLAG_WINDOW_MINIMIZED: c_int = 0x00000200;
+/// Set to maximize window (expanded to monitor)
+pub const FLAG_WINDOW_MAXIMIZED: c_int = 0x00000400;
+/// Set to window non focused
+pub const FLAG_WINDOW_UNFOCUSED: c_int = 0x00000800;
+/// Set to window always on top
+pub const FLAG_WINDOW_TOPMOST: c_int = 0x00001000;
+/// Set to allow windows running while minimized
+pub const FLAG_WINDOW_ALWAYS_RUN: c_int = 0x00000100;
+/// Set to allow transparent framebuffer
+pub const FLAG_WINDOW_TRANSPARENT: c_int = 0x00000010;
+/// Set to support HighDPI
+pub const FLAG_WINDOW_HIGHDPI: c_int = 0x00002000;
+/// Set to support mouse passthrough, only supported when FLAG_WINDOW_UNDECORATED
+pub const FLAG_WINDOW_MOUSE_PASSTHROUGH: c_int = 0x00004000;
+/// Set to try enabling MSAA 4X
+pub const FLAG_MSAA_4X_HINT: c_int = 0x00000020;
+
+/// Set to try enabling interlaced video format (for V3D)pub const FLAG_INTERLACED_HINT: c_int    = 0x00010000;
 pub const KEY_NULL: c_int = 0;
 pub const KEY_APOSTROPHE: c_int = 39;
 pub const KEY_COMMA: c_int = 44;
